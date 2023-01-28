34804376d789c09482e87782d08168e94bdcc24f
Yogen P <yogendran.poonudurai@setel.com>
2023-01-28 20:17:48 +0800

    added buf

--- go.mod
+++ go.mod
@@ -2,7 +2,7 @@ module github.com/firacloudtech/grpc-echo-benchmark
 
 go 1.19
 
-require google.golang.org/protobuf v1.28.1
+require google.golang.org/protobuf v1.28.1 // indirect
 
 require (
 	buf.build/gen/go/grpc-ecosystem/grpc-gateway/protocolbuffers/go v1.28.1-20221127060915-a1ecdc58eccd.4 // indirect
@@ -13,9 +13,8 @@ require (
 )
 
 require (
-	buf.build/gen/go/firacloudtech/grpc-echo-benchmark/grpc/go v1.2.0-20230123160103-0a94928fc203.4
-	buf.build/gen/go/firacloudtech/grpc-echo-benchmark/protocolbuffers/go v1.28.1-20230123162638-21cf7e84720f.4
-	github.com/grpc-ecosystem/grpc-gateway/v2 v2.15.0
-	google.golang.org/genproto v0.0.0-20221207170731-23e4bf6bdc37
+	buf.build/gen/go/firacloudtech/grpc-echo-benchmark/grpc/go v1.2.0-20230122134124-c7efdc22f76a.4
+	buf.build/gen/go/firacloudtech/grpc-echo-benchmark/protocolbuffers/go v1.28.1-20230122134124-c7efdc22f76a.4
+	github.com/grpc-ecosystem/grpc-gateway v1.16.0
 	google.golang.org/grpc v1.51.0
 )
--- grpc-server/main.go
+++ grpc-server/main.go
@@ -6,53 +6,52 @@ import (
 	"log"
 	"net"
 	"net/http"
-	"sync"
 
 	// import module using buf
-
-	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
+	orderv1 "buf.build/gen/go/firacloudtech/grpc-echo-benchmark/protocolbuffers/go/order/v1"
+	productv1 "buf.build/gen/go/firacloudtech/grpc-echo-benchmark/protocolbuffers/go/product/v1"
+	"github.com/grpc-ecosystem/grpc-gateway/runtime"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 
-	orderv1 "github.com/firacloudtech/grpc-echo-benchmark/gen/go/order/v1"
-	productv1 "github.com/firacloudtech/grpc-echo-benchmark/gen/go/product/v1"
+	orderGrpc "buf.build/gen/go/firacloudtech/grpc-echo-benchmark/grpc/go/order/v1/orderv1grpc"
+	productGrpc "buf.build/gen/go/firacloudtech/grpc-echo-benchmark/grpc/go/product/v1/productv1grpc"
 )
 
 var (
 	port        = 5002
 	gatewayPort = 3001
-	wg          sync.WaitGroup
 )
 
 type combinedServer struct {
-	productv1.UnimplementedProductServiceServer
-	orderv1.UnimplementedOrderServiceServer
+	productGrpc.UnimplementedProductServiceServer
+	orderGrpc.UnimplementedOrderServiceServer
 }
 
+func NewServer() *combinedServer {
+	return &combinedServer{}
 func NewServer() *combinedServer {
 	return &combinedServer{}
 }
 
 func main() {
-	wg.Add(2)
 
-	go func() {
-		if err := run(); err != nil {
-			log.Fatal(err)
-		}
-		wg.Done()
-	}()
+	if err := run(); err != nil {
+		log.Fatal(err)
+	}
 
 	// wait till the grpc server is ready
+	done := make(chan struct{})
 
 	go func() {
 
 		if err := runGrpcGateway(); err != nil {
 			log.Fatal(err)
 		}
-		wg.Done()
+		close(done)
 	}()
-	wg.Wait()
+	<-done
+
 }
 
 func run() error {
@@ -66,8 +65,8 @@ func run() error {
 
 	// Register the services
 	server := grpc.NewServer()
-	productv1.RegisterProductServiceServer(server, &combinedServer{})
-	orderv1.RegisterOrderServiceServer(server, &combinedServer{})
+	productGrpc.RegisterProductServiceServer(server, &combinedServer{})
+	orderGrpc.RegisterOrderServiceServer(server, &combinedServer{})
 
 	log.Println("Listening on port", grpcAddr)
 	if err := server.Serve(listener); err != nil {
@@ -83,22 +82,15 @@ func runGrpcGateway() error {
 
 	gwmux := runtime.NewServeMux()
 	gwAddr := fmt.Sprintf("127.0.0.1:%d", gatewayPort)
-
-	conn, err := grpc.DialContext(context.Background(), fmt.Sprintf("127.0.0.1:%d", port), grpc.WithBlock(), grpc.WithTransportCredentials(insecure.NewCredentials()))
-
-	if err != nil {
-		log.Fatalln("Failed to dial server:", err)
-	}
-
-	err = productv1.RegisterProductServiceHandler(context.Background(), gwmux, conn)
-
-	if err != nil {
-		log.Fatalln("Failed to register gateway:", err)
-	}
-
 	gwServer := &http.Server{
 		Addr:    gwAddr,
 		Handler: gwmux,
+	}
+
+	conn, err := grpc.DialContext(context.Background(), fmt.Sprintf("127.0.0.1:%d", port), grpc.WithTransportCredentials(insecure.NewCredentials()), grpc.WithBlock)
+
+	if err != nil {
+		log.Fatalln("Failed to dial server:", err)
 	}
 
 	log.Println("Serving gRPC-gateway on http://localhost:", gatewayPort)
@@ -111,13 +103,13 @@ func runGrpcGateway() error {
 }
 
 // a function that creates a product and return the response
-func (s *combinedServer) CreateProduct(ctx context.Context, req *productv1.CreateProductRequest) (*productv1.CreateProductResponse, error) {
+func (s *combinedServer) CreateProduct(ctx context.Context, req *productv1.CreateRequest) (*productv1.CreateResponse, error) {
 
 	name := req.GetName()
 
 	log.Printf("Got a request to create a product: %v\n", name)
 
-	return &productv1.CreateProductResponse{
+	return &productv1.CreateResponse{
 		Message: "Success",
 	}, nil
 
@@ -129,6 +121,7 @@ func (s *combinedServer) CreateOrder(ctx context.Context, req *orderv1.CreateReq
 
 	log.Printf("Got a request to create a order: %v\n", name)
 
+	return &orderv1.CreateResponse{}, nil
 	return &orderv1.CreateResponse{}, nil
 
 }
